ab551e1f47807be4f2c450b8b7e94a3ece8d7ce9
zeinab-hajhosseini <zeinab.hajhosseini72@gmail.com>
2023-05-18 12:20:10 +0430

    + Implement functions to handle change tabs in qualification sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -49,7 +49,24 @@ skillsHeader.forEach((element) =>{
 })
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', () =>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContents =>{
+            tabContents.classList.remove('qualification__active')
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
