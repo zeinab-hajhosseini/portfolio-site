ade275841e83a0aff7ce8dec72a1d8d7bce0016e
zeinab-hajhosseini <zeinab.hajhosseini72@gmail.com>
2023-05-21 14:43:53 +0430

    + Implement swiper section of main.js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -68,11 +68,29 @@ tabs.forEach(tab =>{
     })
 })
 
-/*==================== SERVICES MODAL ====================*/
-
-
 /*==================== PORTFOLIO SWIPER  ====================*/
-
+const swiper = new Swiper('.portfolio__container', {
+    // Optional parameters
+    //direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
 /*==================== TESTIMONIAL ====================*/
 
